e1aca58fa61561eec21d849849da094bf6ad2ac5
Tehnik762 <yastroitel@gmail.com>
2017-10-07 21:29:23 +0300

    Первое задание

---
+++ homework/homework01.php
@@ -0,0 +1,22 @@
+<?php
+// Task #1
+
+$name = "Антон";
+$age = '35';
+echo "Задание №1" . PHP_EOL;
+echo "Меня зовут: " . $name . PHP_EOL;
+echo 'Мне ' . $age . ' лет' . PHP_EOL;
+echo '“!|\\/’”\\', PHP_EOL;
+
+
+// Task #2
+
+$all_images = 80;
+$marker_images = 23;
+$pencil_images = 40;
+
+$oil_images = $all_images - $marker_images - $pencil_images;
+echo "Задание №2" . PHP_EOL;
+echo "Красками нарисовано " . $oil_images . " картин." . PHP_EOL;
+
+?>
