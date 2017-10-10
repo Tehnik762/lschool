fc9abcdfc92e679477456ba719839bfd0ffcf606
Tehnik762 <yastroitel@gmail.com>
2017-10-10 09:57:40 +0300

    Beautify

--- homework/homework01/homework06.php
+++ homework/homework01/homework06.php
@@ -1,34 +1,33 @@
 <?php
-
 //	Task #6
 
 $bmv = [
-	"model" => "X5",
-	"speed" => 120,
-	"doors" => 5,
-	"year" => "2015"
+    "model" => "X5",
+    "speed" => 120,
+    "doors" => 5,
+    "year" => "2015"
 ];
 
 $toyota = [
-	"model" => "Auris",
-	"speed" => 130,
-	"doors" => 3,
-	"year" => "2011"
+    "model" => "Auris",
+    "speed" => 130,
+    "doors" => 3,
+    "year" => "2011"
 ];
 
 $opel = [
-	"model" => "Vectra",
-	"speed" => 98,
-	"doors" => 5,
-	"year" => "1996"
+    "model" => "Vectra",
+    "speed" => 98,
+    "doors" => 5,
+    "year" => "1996"
 ];
 
-$cars = [$bmv, $toyota, $opel];
+$cars = ["BMV" => $bmv, "Toyota" => $toyota, "Opel" => $opel];
 
-foreach ($cars as $name=>$car) {
-	echo "Car ".$name.PHP_EOL;
-	foreach ($car as $params) {
-	echo $params." ";
-	}
-	echo PHP_EOL;
+foreach ($cars as $name => $car) {
+    echo "Car " . $name . PHP_EOL;
+    foreach ($car as $params) {
+        echo $params . " ";
+    }
+    echo PHP_EOL;
 }
