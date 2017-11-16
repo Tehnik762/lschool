2ef33962985f304460978167cf0611f8e9860f00
Tehnik762 <yastroitel@gmail.com>
2017-11-16 12:12:13 +0300

    Готово к проверке ДЗ5

--- homework/homework05/class.php
+++ homework/homework05/class.php
@@ -1,125 +1,149 @@
 <?php
-
-
-/**
- * Класс описывающий автомобиль
- *
- * 
+/*
+ *  
+ *     Homework PHP LoftSchool
+ *     Student: Braslavskii Anton
+ *     Email: yastroitel@gmail.com
+ *  
  */
-class Car {
+
+class Car
+{
+
     private $engine;
     private $transmission;
 
-
-
-    public function __construct($type, $horseforce) {
+    public function __construct($type, $horseforce)
+    {
         $this->engine = new Engine($horseforce);
         $this->transmission = TransFactory::turnOn($type);
+        renderString("У вашего автомобиля " . $this->transmission->type . " коробка передач");
     }
-    
-    public function move($speed, $dist, $direction) {
-        $this->engine->turnOn($speed);
+
+    public function move($speed, $dist, $direction)
+    {
+        renderString($this->engine->turnOn($speed));
         $gear = $this->transmission->changeState($speed, $direction);
+        renderString("Вы включили " . $gear . " передачу");
         $this->engine->working($dist, $speed);
         $this->engine->turnOff();
         $this->transmission->neutral();
-        
     }
-    
-    
 }
 
-class Engine {
+class Engine
+{
+
     private $power, $temp;
-    
-    public function __construct($hf) {
+
+    public function __construct($hf)
+    {
         $this->power = $hf;
         $this->temp = 0;
     }
-    
-    private function cooling() {
+
+    private function cooling()
+    {
         $this->temp -= 10;
-        if ($this->temp>90) {
+        if ($this->temp > 90) {
             $this->cooling();
         }
     }
-    
-    public function turnOn($speed) {
-        $max = $this->power*2;
-        if ($max>$speed) {
-        return "Двигатель работает";
+
+    public function turnOn($speed)
+    {
+        $max = $this->power * 2;
+        if ($max > $speed) {
+            return "Двигатель работает";
         } else {
             die("Машина не может разогнаться до такой скорости");
         }
     }
-    
-    public function working($dist, $speed) {
-        while ($dist>0) {
+
+    public function working($dist, $speed)
+    {
+        while ($dist > 0) {
             $dist -= $speed;
-            $this->temp += ceil($speed/10)*5;
+            if ($dist < 0) {
+                break;
+            }
+            $this->temp += ceil($speed / 10) * 5;
             if ($this->temp > 90) {
-                echo $this->cooling();
-                echo "<br/>System Cooling<br/>";
+                renderString("Температура двигателя " . $this->temp);
+                $this->cooling();
+                renderString("<b>System Cooling</b>");
             }
-            echo "Осталось ехать ".$dist." метров<br>";
+            renderString("Осталось ехать " . $dist . " метров");
         }
-        
     }
-    
-    public function turnOff() {
-        echo "Приехали. Температура двигателя ".$this->temp."<br/>";
+
+    public function turnOff()
+    {
+        renderString("Приехали. Температура двигателя " . $this->temp);
     }
-    
 }
 
+abstract class BaseTransmission
+{
 
-abstract class BaseTransmission {
     public $type;
-    
-    public function __construct($type) {
-        $this->type = $type;        
+
+    public function __construct($type)
+    {
+        if ($type == "Auto") {
+            $this->type = "Автоматическая";
+        } else {
+            $this->type = "Механическая";
+        }
     }
-    
-    public function neutral() {
-        return 0;        
+
+    public function neutral()
+    {
+        return "Нейтральную";
     }
 }
-        
 
+class ManualTransmission extends BaseTransmission
+{
 
-
-class ManualTransmission extends BaseTransmission {
-    
-    public function changeState($speed, $direction) {
+    public function changeState($speed, $direction)
+    {
         if ($direction == "back") {
-            return -1;
+            return "Заднюю";
         } else {
-            if ($speed>10) {
-                return 2;
+            if ($speed > 10) {
+                return "Вторую";
             } else {
-                return 1;
+                return "Первую";
             }
         }
-        
     }
-    
 }
 
-class AutoTransmission extends BaseTransmission {
-    
-    public function changeState($speed, $direction) {
+class AutoTransmission extends BaseTransmission
+{
+
+    public function changeState($speed, $direction)
+    {
         if ($direction == "back") {
-            return -1;
+            return "Заднюю";
         } else {
-            return 1;
+            return "Переднюю";
         }
     }
 }
 
-class TransFactory {
-    
-    public static function turnOn($type) {
-        $t = $type."Transmission";
+class TransFactory
+{
+
+    public static function turnOn($type)
+    {
+        $t = $type . "Transmission";
         return new $t($type);
     }
+}
+
+function renderString($str)
+{
+    echo $str . "<br/>";
 }
--- homework/homework05/index.php
+++ homework/homework05/index.php
@@ -1,9 +1,33 @@
 <?php
-
+/*
+ *  
+ *     Homework PHP LoftSchool
+ *     Student: Braslavskii Anton
+ *     Email: yastroitel@gmail.com
+ *  
+ */
 
 require_once 'class.php';
-
-$car = new Car("manual", 159);
-$speed = 5;
-$dist = 1000; 
+renderString("<h3>Машина № 1</h3> 159 лс");
+$car = new Car("Manual", 159);
+$speed = 115;
+$dist = 1000;
 $car->move($speed, $dist, "forward");
+
+renderString("<hr/><h3>Машина № 2</h3> 115 лс");
+$speed = 5;
+$dist = 10;
+$car2 = new Car("Auto", "115");
+$car2->move($speed, $dist, "back");
+
+renderString("<hr/><h3>Машина № 3</h3> 30 лс");
+$car3 = new Car("Manual", 30);
+$speed = 5;
+$dist = 100;
+$car3->move($speed, $dist, "forward");
+
+renderString("<hr/><h3>Машина № 4</h3> 30 лс");
+$car4 = new Car("Manual", 30);
+$speed = 500;
+$dist = 100;
+$car4->move($speed, $dist, "forward");
